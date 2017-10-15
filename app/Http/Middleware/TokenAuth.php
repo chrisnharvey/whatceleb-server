595ade1752c33c31f748b26964fb3982bed15058
Unknown <chris@chrisnharvey.com>
2017-10-15 18:36:35 +0100

    Use JSON for middleware errors

--- app/Http/Middleware/TokenAuth.php
+++ app/Http/Middleware/TokenAuth.php
@@ -15,10 +15,15 @@ class TokenAuth
      */
     public function handle($request, Closure $next)
     {
-        if (empty(env('API_TOKEN'))) abort(500);
+        if (empty(env('API_TOKEN'))) {
+            return response()->json([
+                'error' => 'An internal server error occurred'
+            ], 500);
 
         if ($request->header('X-WhatCeleb-Auth') != env('API_TOKEN')) {
-            abort(403);
+            return response()->json([
+                'error' => 'Invalid API token'
+            ], 403);
         }
 
         return $next($request);

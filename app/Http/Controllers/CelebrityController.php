bfc6a682b7d9558c0e2f1c2913c31e4c50f45e90
Unknown <chris@chrisnharvey.com>
2017-10-15 21:16:33 +0100

    Updates to CelebrityController to get additional info

--- app/Http/Controllers/CelebrityController.php
+++ app/Http/Controllers/CelebrityController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Aws\Rekognition\RekognitionClient;
+use Tmdb\ApiToken as TmdbApiToken;
+use Tmdb\Client as TmdbClient;
+use Tmdb\Repository\ConfigurationRepository as TmdbConfig;
+use Tmdb\Helper\ImageHelper;
+use Tmdb\Repository\FindRepository as TmdbFind;
 
 class CelebrityController extends Controller
 {
@@ -17,7 +22,16 @@ class CelebrityController extends Controller
                 'secret' => env('AWS_SECRET')
             ],
         ]);
+
+        $token  = new TmdbApiToken(env('TMDB_KEY'));
+        $this->tmdb = new TmdbClient($token);
+
+        $configRepository = new TmdbConfig($this->tmdb);
+        $config = $configRepository->load();
+        
+        $this->image = new ImageHelper($config);
     }
+
     public function findByFace(Request $request)
     {
         if ( ! $request->hasFile('photo')) {
@@ -49,7 +63,7 @@ class CelebrityController extends Controller
                         'imdb_id' => str_after($url, 'imdb.com/name/')
                     ];
 
-                    break; // Just get the first celeb with an IMDb ID ,we'll add support for multiple celebs later
+                    break 2; // Just get the first celeb with an IMDb ID ,we'll add support for multiple celebs later
 
                 }
             }
@@ -61,6 +75,47 @@ class CelebrityController extends Controller
             ], 404);
         }
 
+        // Get TMDb info
+        $repository = new TmdbFind($this->tmdb);
+        $find = $repository->findBy($profile['imdb_id'], ['external_source' => 'imdb_id']);
+
+        $personLookup = array_first($find->getPersonResults()->getPeople());
+
+        if (empty($personLookup)) {
+            return response()->json([
+                'error' => "Sorry, not sure who that is."
+            ], 404);
+        }
+
+        $repository = new \Tmdb\Repository\PeopleRepository($this->tmdb);
+        $person = $repository->load($personLookup->getId());
+        $person->setKnownFor($personLookup->getKnownFor());
+
+        $knownFor = [];
+
+        foreach ($person->getKnownFor() as $movie) {
+            $knownFor[] = $this->createMovieArray($movie);
+        }
+
+        $profile = array_merge($profile, [
+            'profile_image' => $this->getImage($person->getProfileImage(), 'h632'),
+            'bio' => $person->getBiography(),
+            'known_for' => $knownFor,            
+        ]);
+
         return compact('profile');
+    }
+
+    protected function getImage($path, $size = 'original')
+    {
+        return 'https:'.$this->image->getUrl($path, $size);
+    }
+
+    protected function createMovieArray($movie)
+    {
+        return [
+            'title' => $movie->getTitle(),
+            'poster' => $this->getImage($movie->getPosterPath(), 'w185')
+        ];
     }
 }
